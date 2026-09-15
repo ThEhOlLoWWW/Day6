a7bf77d06f2b5b9e061bb15dce666d662293db66
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-15 16:29:38 +0100

    ex6 done

--- exercices/exercice-06.js
+++ exercices/exercice-06.js
@@ -14,4 +14,12 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let utilisateur = {
+    nom: "Younes",
+    age: 21,
+    estAdmin: false,
+}
+let valeur = Object.values(utilisateur)
+console.log(valeur);
+
