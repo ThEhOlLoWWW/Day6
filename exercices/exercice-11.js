8212887aeccc628b9faf599807a7ddffd5bf7307
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-15 18:10:41 +0100

    ex11 done

--- exercices/exercice-11.js
+++ exercices/exercice-11.js
@@ -14,4 +14,8 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let obj = {a: 1, b: 2}
+let copie = {...obj}
+copie.c = 3;
+console.log(copie);
