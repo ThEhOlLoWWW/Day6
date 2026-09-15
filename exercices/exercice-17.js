d7689b83ceb26415f23dd8c06be8e34c582816ac
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-15 20:25:06 +0100

    ex17 done

--- exercices/exercice-17.js
+++ exercices/exercice-17.js
@@ -14,4 +14,15 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let priceList = [{ article: "PC", prix: "1500$" }, { article: "Souris", prix: "25$" }];
+
+function vraiNumber(tableau){
+    for (let item of tableau){
+        let prixPropre = item.prix.replace("$","");
+        let prixNum = Number(prixPropre);
+        item.prix = prixNum;
+    }
+    return tableau;
+}
+console.log(vraiNumber(priceList));
