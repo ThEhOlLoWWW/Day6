69ea92fbbbebd40da01d75ec441d84c6c9c56ec6
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-15 20:14:09 +0100

    ex16 done

--- exercices/exercice-16.js
+++ exercices/exercice-16.js
@@ -14,4 +14,10 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let objEntreprise = {nom: "hello", employes: [
+    {nom : "Younes"},
+    {nom: "Sarah"},
+],}
+
+console.log(objEntreprise.employes[1].nom);
