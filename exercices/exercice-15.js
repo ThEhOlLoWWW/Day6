c0b7fa1efe952642d35a72d681c38eca74c51ae8
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-15 20:08:50 +0100

    ex15 done

--- exercices/exercice-15.js
+++ exercices/exercice-15.js
@@ -16,4 +16,14 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let oldchaine = '{"id":1, "titre":"Apprendre le JS", "vu":false}';
+
+let obj = JSON.parse(oldchaine);
+
+obj.vu = true;
+
+let newChaine = JSON.stringify(obj);
+
+console.log(newChaine);
+
